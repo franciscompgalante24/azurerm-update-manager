fe096594aa3b3e6bbf53d7e99a8759fa4ad4839f
Francisco Galante <120483444+franciscompgalante24@users.noreply.github.com>
2024-04-05 22:27:37 +0100

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -18,6 +18,53 @@ variable "location" {
   type        = string
 }
 
+variable "vnet" {
+  description = "Details about the vnet"
+  type = object({
+    name          = string
+    address_space = list(string)
+  })
+}
+
+variable "subnet" {
+  description = "Details about the subnet"
+  type = object({
+    name              = string
+    address_prefixes  = list(string)
+    service_endpoints = list(string)
+  })
+}
+
+variable "nic" {
+  description = "Details about the nic"
+  type = object({
+    name = string
+    ip_configuration = object({
+      name                          = string
+      private_ip_address_allocation = string
+    })
+  })
+}
+
+variable "linux_vm" {
+  description = "Details about the Linux VM"
+  type = object({
+    name           = string
+    size           = string
+    admin_username = string
+    os_disk = object({
+      caching              = string
+      storage_account_type = string
+    })
+    source_image_reference = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
+  })
+}
+
 variable "automation_account_name" {
   description = "Name of the Automation Account"
   type        = string
